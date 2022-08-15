b7045ed6b1d709dab15d151c1aa0d1b40bf8f26c
Carlos Sánchez <oceanrdn@gmail.com>
2022-08-15 18:50:45 +0200

    Remove mnemonic file cp

--- install.js
+++ install.js
@@ -1,32 +1,35 @@
 var fs = require("fs");
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
 
 const challenges = JSON.parse(fs.readFileSync("challenges.json").toString());
 
-const setupChallenge = async challenge => {
+const setupChallenge = async (challenge) => {
   try {
     if (!fs.existsSync(`./${challenge.name}`)) {
       console.log("====] CLONING " + challenge.name + "[==============]");
-      const result1 = await exec('git clone -b ' + challenge.name + ' ' + challenge.github + ' ' + challenge.name);
+      const result1 = await exec(
+        "git clone -b " +
+          challenge.name +
+          " " +
+          challenge.github +
+          " " +
+          challenge.name
+      );
       console.log(`stdout: ${result1.stdout}\n`);
       console.log(`stderr: ${result1.stderr}\n`);
     }
 
     console.log("====] UPDATING " + challenge.name + "[==============]");
-    const result2 = await exec('cd ' + challenge.name + ' && git pull && yarn install');
+    const result2 = await exec(
+      "cd " + challenge.name + " && git pull && yarn install"
+    );
     console.log(`stdout: ${result2.stdout}\n`);
     console.log(`stderr: ${result2.stderr}\n`);
-
-    console.log("====] INSTALLING "+challenge.name+" MNEMONIC[==============]")
-    const result3 = await exec('cp mnemonic.txt ' + challenge.name + '/packages/hardhat/');
-    console.log(`stdout: ${result3.stdout}\n`);
-    console.log(`stderr: ${result3.stderr}\n`);
-
-  } catch(e){
+  } catch (e) {
     console.log(e);
   }
-}
+};
 
 (async () => {
   for (let c in challenges) {
@@ -35,4 +38,3 @@ const setupChallenge = async challenge => {
     await setupChallenge(challenge);
   }
 })();
-
