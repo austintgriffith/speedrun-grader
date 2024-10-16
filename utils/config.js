66e28eb1f7b835344e1fdaf4ca262886444c1590
Rinat <rinat@hey.com>
2024-10-16 17:41:49 +0200

    More testnets support (#10)
    Co-authored-by: Carlos Sánchez <oceanrdn@gmail.com>

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,24 @@
-const allowedNetworks = ["goerli", "sepolia"];
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const VALID_BLOCK_EXPLORER_HOSTS = [
+  "sepolia.etherscan.io",
+  "sepolia-optimism.etherscan.io",
+];
+
+const API_CONFIG = {
+  "sepolia.etherscan.io": {
+    url: "https://api-sepolia.etherscan.io/api",
+    key: process.env.ETHERSCAN_API_KEY,
+  },
+  "sepolia-optimism.etherscan.io": {
+    url: "https://api-sepolia-optimistic.etherscan.io/api",
+    key: process.env.ETHERSCAN_OPTIMISTIC_API_KEY,
+  },
+};
+
+const getContractCodeUrl = (blockExplorer, address) => {
+  return `${API_CONFIG[blockExplorer].url}?module=contract&action=getsourcecode&address=${address}&apikey=${API_CONFIG[blockExplorer].key}`;
+};
 
 module.exports = {
-  allowedNetworks,
-  ETHERSCAN_API_KEY,
+  VALID_BLOCK_EXPLORER_HOSTS,
+  getContractCodeUrl,
 };
