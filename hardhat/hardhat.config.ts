c33a92d0054adb868645a6185403cbff078c5902
Carlos Sánchez <oceanrdn@gmail.com>
2025-10-20 16:16:15 +0200

    add zk challenge

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -17,6 +17,17 @@ const config: HardhatUserConfig = {
           },
         },
       },
+      {
+        version: "0.8.27",
+        settings: {
+          viaIR: true,
+          optimizer: {
+            enabled: true,
+            // https://docs.soliditylang.org/en/latest/using-the-compiler.html#optimizer-options
+            runs: 200,
+          },
+        },
+      },
     ],
   },
 };
